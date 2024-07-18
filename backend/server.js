3b84ad5fe8a7da770613de1711d309af7bb3de1c
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-18 14:41:52 +0530

    18th July 2024

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@
 import express from "express";
 import dotenv from "dotenv";
 const app = express();
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
-// import messageRoutes from "./routes/message.routes.js";
-// import userRoutes from "./routes/user.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 // import { app, server } from "./socket/socket.js";
@@ -18,11 +18,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
