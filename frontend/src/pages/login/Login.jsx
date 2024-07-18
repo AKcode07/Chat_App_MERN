767430f87d864491ed9b9f615042400a839221c1
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-18 22:09:49 +0530

    18th July 2024

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -14,8 +14,8 @@ const Login = () => {
 	};
 
 	return (
-		<div className='flex flex-col items-center justify-center min-h-screen px-4 sm:px-0'>
-			<div className='w-full max-w-md p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Login
 					<span className='text-blue-500'> ChatApp</span>
@@ -47,7 +47,7 @@ const Login = () => {
 							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
-					<Link to='/signup' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
 					</Link>
 
@@ -61,5 +61,4 @@ const Login = () => {
 		</div>
 	);
 };
-
 export default Login;
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -24,8 +24,8 @@ const SignUp = () => {
 	};
 
 	return (
-		<div className='flex flex-col items-center justify-center min-h-screen px-4 sm:px-0'>
-			<div className='w-full max-w-md p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Sign Up <span className='text-blue-500'> ChatApp</span>
 				</h1>
@@ -38,14 +38,14 @@ const SignUp = () => {
 						<input
 							type='text'
 							placeholder='John Doe'
-							className='w-full input input-bordered h-10'
+							className='w-full input input-bordered  h-10'
 							value={inputs.fullName}
 							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
 						/>
 					</div>
 
 					<div>
-						<label className='label p-2'>
+						<label className='label p-2 '>
 							<span className='text-base label-text'>Username</span>
 						</label>
 						<input
@@ -85,12 +85,16 @@ const SignUp = () => {
 
 					<GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
 
-					<Link to='/login' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link
+						to={"/login"}
+						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
+						href='#'
+					>
 						Already have an account?
 					</Link>
 
 					<div>
-						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
 							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
 						</button>
 					</div>
@@ -99,5 +103,4 @@ const SignUp = () => {
 		</div>
 	);
 };
-
 export default SignUp;
