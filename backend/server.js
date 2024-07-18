07ff014c5ec108f9528f7d75f2f575152a0360aa
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-18 21:22:07 +0530

    18th July 2024

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
-// import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -9,11 +8,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-// import { app, server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
@@ -24,13 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
